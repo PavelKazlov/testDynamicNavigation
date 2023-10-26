763d8032f420c71f9d9cfec57c0175c2d88c1b2f
C.S.F.S <111419091+PavelKazlov@users.noreply.github.com>
2023-10-26 08:00:56 +0200

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 120,
+  semi: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  singleQuote: true,
+};
---
+++ src/components/CustomButton/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button, Text, View } from "react-native";
+
+export const CustomButton = () => {
+  return (
+    <View>
+      <Button title="button" onPress={() => {}} />
+
+      <Text>Hello!!!</Text>
+    </View>
+  );
+};
